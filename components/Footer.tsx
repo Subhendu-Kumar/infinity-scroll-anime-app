953ed673ae971b00f791e5643a7a37bf2137bd82
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-04-01 14:15:11 +0530

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="sm:px-16 py-4 px-8 flex justify-evenly items-center gap-2 flex-wrap bg-[#4b3f52]">
+      <p className="text-base font-bold text-white">
+        &copy; {currentYear} Subhendu Kumar
+      </p>
+      <Image
+        src="/./logo.png"
+        alt="logo"
+        width={47}
+        height={44}
+        className="object-contain"
+      />
+      <div className="flex items-center gap-6">
+        <a
+          href="https://www.linkedin.com/in/subhendu-kumar-dutta/"
+          target="_blank"
+          className="font-semibold"
+        >
+          Linkedin
+        </a>
+        <a
+          href="https://twitter.com/Subhendu_330"
+          target="_blank"
+          className="font-semibold"
+        >
+          Twitter
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
