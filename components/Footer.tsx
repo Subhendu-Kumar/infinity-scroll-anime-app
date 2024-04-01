c9b32cf20de136c65e52ea9a8574a50bf7743224
Subhendu-Kumar <subhendukumardutta330@gmail.com>
2024-04-01 20:36:51 +0530

    modified 3rd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="sm:px-16 py-4 px-8 flex justify-evenly items-center gap-2 flex-wrap bg-[#4b3f52]">
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#4b3f52]">
       <p className="text-base font-bold text-white">
         &copy; {currentYear} Subhendu Kumar
       </p>
       <Image
-        src="/./logo.png"
+        src="/./logo.svg"
         alt="logo"
         width={47}
         height={44}
         className="object-contain"
       />
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-20">
         <a
           href="https://www.linkedin.com/in/subhendu-kumar-dutta/"
           target="_blank"
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,43 +1,110 @@
 "use client";
 
-import { fetchAnime } from "@/app/action";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+// import { fetchAnime } from "@/app/action";
+// import Image from "next/image";
+// import { useEffect, useState } from "react";
+// import { useInView } from "react-intersection-observer";
+// import AnimeCard from "./AnimeCard";
+
+// let page = 2;
+
+// export type AnimeCard = JSX.Element;
+
+// function LoadMore() {
+//   const { ref, inView } = useInView();
+//   const [data, setData] = useState<AnimeCard[]>([]);
+
+//   useEffect(() => {
+//     if (inView) {
+//       fetchAnime(page).then((res) => {
+//         setData([...data, ...res]);
+//         page++;
+//       });
+//     }
+//   }, [data, inView]);
+//   return (
+//     <>
+//       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+//         {data}
+//       </section>
+//       <section className="flex justify-center items-center w-full">
+//         <div ref={ref}>
+//           <Image
+//             src="./spinner.svg"
+//             alt="spinner"
+//             width={56}
+//             height={56}
+//             className="object-contain"
+//           />
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default LoadMore;
+
+import React, { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import Image from "next/image";
+import { fetchAnime } from "@/app/action";
 import AnimeCard from "./AnimeCard";
 
-let page = 2;
-
 export type AnimeCard = JSX.Element;
-
 function LoadMore() {
   const { ref, inView } = useInView();
   const [data, setData] = useState<AnimeCard[]>([]);
+  const [page, setPage] = useState(2);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (inView) {
+    if (inView && !isLoading) {
+      setIsLoading(true);
       fetchAnime(page).then((res) => {
-        setData([...data, ...res]);
-        page++;
+        setData((prevData) => [...prevData, ...res]);
+        setPage((prevPage) => prevPage + 1);
+        setIsLoading(false);
       });
     }
-  }, [data, inView]);
+  }, [inView, page, isLoading]);
+
+  const handleLoadMore = () => {
+    setIsLoading(true);
+    fetchAnime(page).then((res) => {
+      setData((prevData) => [...prevData, ...res]);
+      setPage((prevPage) => prevPage + 1);
+      setIsLoading(false);
+    });
+  };
+
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {data}
       </section>
-      <section className="flex justify-center items-center w-full">
-        <div ref={ref}>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
-        </div>
-      </section>
+      {page <= 15 && (
+        <section className="flex justify-center items-center w-full">
+          <div ref={ref}>
+            <Image
+              src="/spinner.svg"
+              alt="spinner"
+              width={56}
+              height={56}
+              className="object-contain"
+            />
+          </div>
+        </section>
+      )}
+      {page > 15 && (
+        <section className="flex justify-center items-center w-full">
+          <button
+            onClick={handleLoadMore}
+            className="bg-stone-300 text-black p-2 rounded font-semibold"
+          >
+            Load More
+          </button>
+        </section>
+      )}
     </>
   );
 }
