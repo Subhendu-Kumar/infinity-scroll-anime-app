86de9ba92495339d532c0b95cb60fa88d9c88bc2
Subhendu Kumar <158295332+Subhendu-Kumar@users.noreply.github.com>
2024-04-01 20:45:30 +0530

    Update LoadMore.tsx

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,49 +1,5 @@
 "use client";
 
-// import { fetchAnime } from "@/app/action";
-// import Image from "next/image";
-// import { useEffect, useState } from "react";
-// import { useInView } from "react-intersection-observer";
-// import AnimeCard from "./AnimeCard";
-
-// let page = 2;
-
-// export type AnimeCard = JSX.Element;
-
-// function LoadMore() {
-//   const { ref, inView } = useInView();
-//   const [data, setData] = useState<AnimeCard[]>([]);
-
-//   useEffect(() => {
-//     if (inView) {
-//       fetchAnime(page).then((res) => {
-//         setData([...data, ...res]);
-//         page++;
-//       });
-//     }
-//   }, [data, inView]);
-//   return (
-//     <>
-//       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-//         {data}
-//       </section>
-//       <section className="flex justify-center items-center w-full">
-//         <div ref={ref}>
-//           <Image
-//             src="./spinner.svg"
-//             alt="spinner"
-//             width={56}
-//             height={56}
-//             className="object-contain"
-//           />
-//         </div>
-//       </section>
-//     </>
-//   );
-// }
-
-// export default LoadMore;
-
 import React, { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import Image from "next/image";
@@ -82,7 +38,7 @@ function LoadMore() {
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
         {data}
       </section>
-      {page <= 15 && (
+      {page <= 8 && (
         <section className="flex justify-center items-center w-full">
           <div ref={ref}>
             <Image
@@ -95,7 +51,7 @@ function LoadMore() {
           </div>
         </section>
       )}
-      {page > 15 && (
+      {page > 8 && (
         <section className="flex justify-center items-center w-full">
           <button
             onClick={handleLoadMore}
